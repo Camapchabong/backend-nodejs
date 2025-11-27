314394411e47ebe1df4e80445250b2fd1ba4ea44
Quy <lequy140305@gmail.com>
2025-11-27 13:17:34 +0700

    Initializing the project, set up environment

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import 'dotenv/config'
+import webRoutes from "./routes/web";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+//config routers
+webRoutes(app);
+
+//config static files: images/css/js
+app.use(express.static('public'));
+
+
+app.listen(PORT, () => {
+    console.log(`My app is running on port: ${PORT}`)
+    console.log("env port: ", process.env.PORT)
+})
+
