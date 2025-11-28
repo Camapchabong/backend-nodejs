f8f5d77c0ad31bf5e398dc78a350da52af61df47
Quy <lequy140305@gmail.com>
2025-11-28 11:39:51 +0700

    Add function delete user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getAllUsers, handleCreateUser } from "services/user.service";
+import { getAllUsers, handleCreateUser, handleDeleteUser } from "services/user.service";
 
 const getHomePage = async (req: Request, res: Response) => {
     //Get users
@@ -15,16 +15,23 @@ const getCreateUserPage = (req: Request, res: Response) => {
 }
 
 const postCreateUser = async (req: Request, res: Response) => {
-    const { yourName, email, address } = req.body;
+    const { fullName, email, address } = req.body;
     
     //Handle create user
-    await handleCreateUser(yourName, email, address);
+    await handleCreateUser(fullName, email, address);
 
+    return res.redirect("/"); //Return data to client
+}
+
+const postDeleteUser = async (req: Request, res: Response) => {
+    const {id} = req.params;
+    await handleDeleteUser(id);
     return res.redirect("/"); //Return data to client
 }
 
 export {
     getHomePage,
     getCreateUserPage,
-    postCreateUser
+    postCreateUser,
+    postDeleteUser
 };
